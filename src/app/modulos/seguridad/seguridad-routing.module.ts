ac6f14eda0dba04683bdc8187c8ffe0c2a914f71
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2023-04-24 13:54:26 -0500

    Se definen los cambios y actualizaciones con respecto al enrutamiento

--- src/app/modulos/seguridad/seguridad-routing.module.ts
+++ src/app/modulos/seguridad/seguridad-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IdentificacionUsuarioComponent } from './identificacion-usuario/identificacion-usuario.component';
+import { CambioClaveComponent } from './cambio-clave/cambio-clave.component';
+import { RecuperacionClaveComponent } from './recuperacion-clave/recuperacion-clave.component';
+import { CerrarSesionComponent } from './cerrar-sesion/cerrar-sesion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "identificar-usuario",
+    component: IdentificacionUsuarioComponent
+  },
+  {
+    path: "cambiar-clave",
+    component: CambioClaveComponent
+  },
+  {
+    path: "recuperar-clave",
+    component: RecuperacionClaveComponent
+  },
+  {
+    path: "cerrar-sesion",
+    component: CerrarSesionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
