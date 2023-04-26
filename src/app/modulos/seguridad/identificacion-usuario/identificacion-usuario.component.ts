3755b06aeaa2c9d60faefd7bfafaed8ac0d648de
paula-cano <paula.14canob@gmail.com>
2023-04-25 19:36:03 -0500

    cambios en identificar-usuario; creacion de metodos construirFormulario, identificarUsuario. Creacion del servicio de seguridad

--- src/app/modulos/seguridad/identificacion-usuario/identificacion-usuario.component.ts
+++ src/app/modulos/seguridad/identificacion-usuario/identificacion-usuario.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-identificacion-usuario',
@@ -6,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./identificacion-usuario.component.css']
 })
 export class IdentificacionUsuarioComponent {
+  fGroup: FormGroup = new FormGroup({});
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.ConstruirFormulario();
+  }
+
+  ConstruirFormulario() {
+    this.fGroup = this.fb.group({
+      usuario: ['', [Validators.required, Validators.email]],
+      clave: ['', [Validators.required,]]
+    });
+  }
+
+  IdentificarUsuario() {
+    if (this.fGroup.invalid) {
+      alert("Datos incompletos");
+    } else {
+      let usuario = this.obtenerFormGroup['usuario'].value;
+      let clave = this.obtenerFormGroup['clave'].value;
+    }
+  }
+
+  get obtenerFormGroup() {
+    return this.fGroup.controls;
+  }
 
 }
