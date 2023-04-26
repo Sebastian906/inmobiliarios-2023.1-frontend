4d9d99c92a3fe79ec9866664300e7a51a0dfe041
paula-cano <paula.14canob@gmail.com>
2023-04-25 19:45:23 -0500

    creacion de la carpeta modelos y del archivo usuario, adicion del metodo identificar usuario en el servicio de seguridad

--- src/app/servicios/seguridad.service.ts
+++ src/app/servicios/seguridad.service.ts
@@ -6,4 +6,23 @@ import { Injectable } from '@angular/core';
 export class SeguridadService {
 
   constructor() { }
+
+  urlBase: string = ConfiguracionRutasBackend.urlSeguridad;
+  constructor(private http: HttpClient) {
+    this.validacionDeSesion();
+  }
+
+
+  /**
+   * Identificar usuario
+   * @param usuario 
+   * @param clave 
+   * @returns datos del usuario validado
+   */
+  IdentificarUsuario(usuario: string, clave: string): Observable<UsuarioModel> {
+    return this.http.post<UsuarioModel>(`${this.urlBase}identificar-usuario`, {
+      correo: usuario,
+      clave: clave
+    });
+  }
 }
