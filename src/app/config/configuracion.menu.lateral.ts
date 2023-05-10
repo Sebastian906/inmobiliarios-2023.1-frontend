05d4c681e8996499bec184b5dfdef9a2c435a26a
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2023-05-10 15:15:42 -0500

    primera parte de la visualización del menú lateral con permisos

---
+++ src/app/config/configuracion.menu.lateral.ts
@@ -0,0 +1,22 @@
+export namespace ConfiguracionMenuLateral {
+  export const listaMenus = [
+    {
+      id: "6425b8236df38129fc076f5c",
+      ruta: "/seguridad/usuario-listar",
+      icono: "<i class='material-icons'>cloud</i>",
+      texto: "Usuarios"
+    },
+    {
+      id: "6425ac25a9803b01840bd1f9",
+      ruta: "/seguridad/rol-listar",
+      icono: "<i class='material-icons'>cloud</i>",
+      texto: "Roles"
+    },
+    {
+      id: "64333a67786d1a179c569ca2",
+      ruta: "/parametros/inmueble-listar",
+      icono: "<i class='material-icons'>cloud</i>",
+      texto: "Productos"
+    }
+  ];
+}
--- src/app/modelos/usuario.validado.model.ts
+++ src/app/modelos/usuario.validado.model.ts
@@ -1,6 +1,8 @@
 import { UsuarioModel } from "./usuario.model";
+import { PermisoModel } from "./permiso.model";
 
 export class UsuarioValidadoModel{
     user?: UsuarioModel;
     token?: string ="";
+    menu: PermisoModel[] = []
 }
