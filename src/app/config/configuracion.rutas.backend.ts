48e723ebe539a1f5d530d7c1e9ed669ca659f0f3
Valentina Acosta <124893622+ValentinaAcosta@users.noreply.github.com>
2023-05-14 21:53:44 -0500

    Formulario asesor ajuste errores

--- src/app/config/configuracion.rutas.backend.ts
+++ src/app/config/configuracion.rutas.backend.ts
@@ -1,9 +1,5 @@
 export namespace ConfiguracionRutasBackend{
     export const urlSeguridad:string = "http://localhost:3000/";
-<<<<<<< HEAD
     export const urlParametros:string = "http://localhost:3001/";
 }
-=======
-    export const urlNegocio:string = "http://localhost:3001/";
-}
->>>>>>> d3645eaab4474b305f95c725c8af3da18c1298c9
+   
